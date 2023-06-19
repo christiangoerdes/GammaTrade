f5928831bc112ee54feab8510c2a16b5e248df7e
zuevs0 <s68szuev@uni-bonn.de>
2023-06-19 18:51:24 +0200

    Item-Klasse angelegt

--- Server/include/item.hpp
+++ Server/include/item.hpp
@@ -5,10 +5,24 @@
 
 // Item class 
 class Item {
-    public: 
+    public:
         // Constructor 
-        Item(std::string name, double price);
-        
+        Item(std::string name, int quantity) {
+            _name = name;
+            _quantity = quantity;
+        }
+
+        int getQuantity() const {
+            return _quantity;
+        }
+
+        void changeQuantity(int change) {
+            _quantity += change;
+        }
+
+    private:
         // Attributes 
-        std::string name; 
+        std::string _name;
+        int _quantity;
+
 };
