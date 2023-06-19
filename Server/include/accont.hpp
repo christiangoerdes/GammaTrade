01e6adb1a2d4907504a2f6e0fe108f8896a1c90b
Christian Gördes <christian.goerdes@outlook.de>
2023-06-19 17:16:35 +0200

    implemented account

--- Server/include/accont.hpp
+++ Server/include/accont.hpp
@@ -1,1 +1,17 @@
 #pragma once 
+#include "item.hpp"
+#include <iostream>
+#include <vector>
+
+// Account class 
+class Account {
+    public: 
+        // Constructor 
+        Item(std::string password, std::string owner, double balance);
+        
+        // Attributes 
+        std::string name; 
+        std::string password; 
+        double balance; 
+        std::vector<Item> goods; 
+}; 
