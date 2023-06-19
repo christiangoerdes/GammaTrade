6c8cf1cfaa6a92e8659be91117a6fff70efd0aa3
Christian Gördes <christian.goerdes@outlook.de>
2023-06-19 20:20:17 +0200

    refactoring

--- Server/include/account.hpp
+++ Server/include/account.hpp
@@ -9,62 +9,19 @@
 class Account {
     public: 
         // Constructor 
-        Account(std::string name, std::string password) {
-            _name = name;
-            _password = password;
-            _balance = 0;
-        }
+        Account(std::string name, std::string password);
         
         // Update the account balance
-        bool add_to_balance(int amount) {
-            if(amount < 0) {
-                return false;
-            }
-            _balance += amount;
-            return true;
-        }
-        bool take_from_balance(int amount) {
-            if(amount > _balance || amount < 0) {
-                return false;
-            }
-            _balance -= amount;
-            return true;
-        }
-        bool buy_stock(std::string stock, int quantity, int price) {
-            if(price > _balance) {
-                return false;
-            }
-            if(_stocks.find(stock) != _stocks.end()) {
-                _stocks[stock] += quantity;
-            }else{
-                _stocks[stock] = quantity;
-            }
-            _balance -= price;
-            return true;
-        }
+        bool add_to_balance(int amount);
+        bool take_from_balance(int amount);
+        bool buy_stock(std::string stock, int quantity, int price);
 
-        bool sell_stock(std::string stock, int quantity, int price) {
-
-            if(_stocks.find(stock) == _stocks.end() || quantity > _stocks[stock]) {
-                return false;
-            }
-
-            _stocks[stock] -= quantity;
-            _balance += price;
-            return true;
-
-        }
+        bool sell_stock(std::string stock, int quantity, int price);
 
         // Getter 
-        std::string& get_name() {
-            return _name;
-        }
-        std::string& get_password() {
-            return _password;
-        }
-        double& get_balance() {
-            return _balance;
-        }
+        std::string& get_name();
+        std::string& get_password();
+        double& get_balance();
 
     private: 
         // Attributes 
---
+++ Server/src/account.cpp
@@ -0,0 +1,56 @@
+#include "../include/account.hpp"
+
+Account::Account(std::string name, std::string password) {
+    _name = name;
+    _password = password;
+    _balance = 0;
+}
+
+// Update the account balance
+bool Account::add_to_balance(int amount) {
+    if(amount < 0) {
+        return false;
+    }
+    _balance += amount;
+    return true;
+}
+bool Account::take_from_balance(int amount) {
+    if(amount > _balance || amount < 0) {
+        return false;
+    }
+    _balance -= amount;
+    return true;
+}
+bool Account::buy_stock(std::string stock, int quantity, int price) {
+    if(price > _balance) {
+        return false;
+    }
+    if(_stocks.find(stock) != _stocks.end()) {
+        _stocks[stock] += quantity;
+    }else{
+        _stocks[stock] = quantity;
+    }
+    _balance -= price;
+    return true;
+}
+
+bool Account::sell_stock(std::string stock, int quantity, int price) {
+    if(_stocks.find(stock) == _stocks.end() || quantity > _stocks[stock]) {
+        return false;
+    }
+    _stocks[stock] -= quantity;
+    _balance += price;
+    return true;
+
+}
+
+// Getter 
+std::string& Account::get_name() {
+    return _name;
+}
+std::string& Account::get_password() {
+    return _password;
+}
+double& Account::get_balance() {
+    return _balance;
+}
