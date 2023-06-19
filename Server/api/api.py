f68dc023dbf3ceff6b175fb845f00d20474579a2
Christian Gördes <christian.goerdes@outlook.de>
2023-06-19 16:40:51 +0200

    added fastapi imports

--- Server/api/api.py
+++ Server/api/api.py
@@ -3,3 +3,24 @@ try:
 except ImportError as e:
     print(f"The import caused the following exception: {e}")
     exit(1)
+
+import os
+from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+# initialize marketplace
+b = GammaTrade()
+
+# initialize API
+api = FastAPI()
+
+# Enable access from other devices
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+  
