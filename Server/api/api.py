80051679fba2509a80d241f8f3bff4b83d771e74
Christian Gördes <christian.goerdes@outlook.de>
2023-06-20 00:21:19 +0200

    .

--- Server/api/api.py
+++ Server/api/api.py
@@ -9,9 +9,10 @@ from gammaTrade import GammaTrade
 from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 
 # initialize marketplace
-g = GammaTrade(100)
+g = GammaTrade(1)
 
 # initialize API
 api = FastAPI()
@@ -78,6 +79,7 @@ async def getStocks():
 async def getMyStocks(name: str, password: str):
     return {"stocks": get_my_stocks(name, password)}
 
+
 def get_stocks():
     s = g.get_stocks()
     stocks = []
@@ -99,3 +101,58 @@ def get_my_stocks(name, password):
         }
         stocks.append(stock_obj)
     return stocks
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def kursverlauf(tendenz, streuung, dt, start, anzahl):
+    sqdt = np.sqrt(dt)
+    kurs = np.zeros(anzahl)
+    kurs[0] = start
+    for i in range(anzahl - 1):
+        Y = 2 * np.random.rand() - 1.0
+        kurs[i + 1] = kurs[i] * (1 + tendenz * dt + streuung * sqdt * Y)
+    return kurs
+
+np.random.seed()
+
+def generate_plots(objects):
+    plots = []
+    for i, obj in enumerate(objects):
+        start = obj.get('start', 5.0 * (2 * np.random.rand() - 1.0))
+        drift = obj.get('drift', 0.3 * (2 * np.random.rand() - 1.0))
+        tendenz = obj.get('tendenz', drift)
+        streuung = obj.get('sd', 0.8)
+        dt = obj.get('dt', 0.001)
+        anzahl = obj.get('anzahl', 1000)
+
+        kurs = kursverlauf(tendenz=tendenz, streuung=streuung, dt=dt, start=start, anzahl=anzahl)
+        plt.plot(kurs)
+        plt.title(f'Object {i+1}')
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.grid(True)
+        plot_path = f'object_{i+1}.png'
+        plt.savefig(plot_path)
+        plots.append(plot_path)
+        plt.clf()  # Clear the figure for the next plot
+    return plots
+
+objects = [
+    {'start': 2.0, 'drift': 0.5, 'tendenz': 0.5, 'sd': 0.6, 'dt': 0.001, 'anzahl': 1000},
+    {'start': -1.0, 'drift': -0.3, 'tendenz': -0.2, 'sd': 0.7, 'dt': 0.001, 'anzahl': 1000},
+    # Add more custom objects as needed
+]
+
+get_stock_details():
+    
+
+image_paths = generate_plots(objects)
+print(image_paths)
+
+@api.get("/foo")
+async def foo():
+    return [FileResponse(path) for path in image_paths][1]
