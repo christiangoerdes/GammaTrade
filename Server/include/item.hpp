7480b35ed84015c99ce0170b99d7d27467292995
Christian Gördes <christian.goerdes@outlook.de>
2023-06-19 17:16:25 +0200

    implemented item

--- Server/include/item.hpp
+++ Server/include/item.hpp
@@ -1,1 +1,14 @@
 #pragma once 
+
+#include <iostream>
+
+// Item class 
+class Item {
+    public: 
+        // Constructor 
+        Item(std::string name, double price, std::string owner);
+        
+        // Attributes 
+        std::string name; 
+        double price; 
+}; 
