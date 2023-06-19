52d07760b5741d5e55c2abfd34f87f70bf67251e
Christian Gördes <christian.goerdes@outlook.de>
2023-06-19 18:46:42 +0200

    added sign up method

--- Server/include/gammaTrade.hpp
+++ Server/include/gammaTrade.hpp
@@ -11,6 +11,7 @@ class GammaTrade {
         // Constructor 
         GammaTrade();
 
+        bool sign_up(std::string name, std::string password);
 
     private: 
         std::vector<Item> items_for_sale; 
--- Server/src/gammaTrade.cpp
+++ Server/src/gammaTrade.cpp
@@ -8,6 +8,18 @@ GammaTrade::GammaTrade() {
 
 }
 
+bool GammaTrade::sign_up(std::string name, std::string password) {
+    // Check if an account with this name already exists
+    for(Account account : accounts) {
+        if(account.name == name) {
+            return false; 
+        }
+    }
+    accounts.push_back(Account(name, password)); 
+    return true; 
+}
+
+
 // Implementation for the Item class
 
 /*
