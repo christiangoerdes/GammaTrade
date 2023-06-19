08db2faab02f797cf63162b444dd14818f14b7b1
Christian Gördes <christian.goerdes@outlook.de>
2023-06-19 16:56:22 +0200

    added function base

--- Server/api/api.py
+++ Server/api/api.py
@@ -27,29 +27,67 @@ api.add_middleware(
 
 # Accout section
 
+# sign up endpoint 
 @api.post("/sign-up")
 async def signUp(name: str, password: str):
+    return {
 
+    }
+
+
+# login endpoint 
 @api.get("/login")
 async def login(name: str, password: str):
+    return {
+        
+    }
 
+
+# deposit endpoint 
 @api.put("/deposit")
-async def deposit(name: str, password: str):
+async def deposit(name: str, password: str, amount: float):
+    return {
+        
+    }
 
+
+# withdraw endpoint 
 @api.put("/withdraw")
-async def withdraw(name: str, password: str):
+async def withdraw(name: str, password: str, amount: float):
+    return {
+        
+    }
 
+
+# endpoint to add an item 
 @api.post("/add-item")
-async def additem(name: str, password: str):
+async def additem(name: str, password: str, itemName: str, itemPrice : float):
+    return {
+        
+    }
 
 
 # Market section
 
+# endpoint to get all items up for sale
 @api.get("/market")
 async def market():
+    return {
+        
+    }
 
+
+# endpoint to sell an item 
 @api.put("/sell")
-async def sell(name: str, password: str):
+async def sell(name: str, password: str, itemName: str):
+    return {
+        
+    }
 
+
+# endpoint to buy an item 
 @api.put("/buy")
-async def buy(name: str, password: str):
+async def buy(name: str, password: str, itemName: str):
+    return {
+        
+    }
