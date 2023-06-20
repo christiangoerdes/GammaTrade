2a3e81d7ea3dcf5f9e420bd22992939a5a6d547c
Christian Gördes <christian.goerdes@outlook.de>
2023-06-20 17:23:48 +0200

    added comments

--- Server/include/stock.hpp
+++ Server/include/stock.hpp
@@ -4,26 +4,58 @@
 #include <list>
 #include <random>
 
+/**
+ * @brief Represents a stock with price history.
+ */
 class Stock {
-    public:
-        Stock(const std::string& name, const double start, const double mean, const double stddev, const int timespan, unsigned seed);
-        Stock();
+public:
+    /**
+     * @brief Constructs a Stock object.
+     * @param name The name of the stock.
+     * @param start The initial price of the stock.
+     * @param mean The mean value for generating price changes.
+     * @param stddev The standard deviation for generating price changes.
+     * @param timespan The number of price updates to simulate.
+     * @param seed The seed value for the random number generator.
+     */
+    Stock(const std::string& name, const double start, const double mean, const double stddev, const int timespan, unsigned seed);
 
-        void update();
+    /**
+     * @brief Default constructor for Stock.
+     */
+    Stock();
 
-        const std::list<double>& getPriceHistory() const;
+    /**
+     * @brief Updates the stock price by generating a new price based on the mean and standard deviation.
+     */
+    void update();
 
-        const double getPrice() const;
-        const std::string getName() const;
-        
-    private:
-        std::string _name;
-        double _start;
-        double _mean;
-        double _stddev;
-        int _timespan;
-        std::list<double> _price_history;
+    /**
+     * @brief Retrieves the price history of the stock.
+     * @return A constant reference to the list of historical prices.
+     */
+    const std::list<double>& getPriceHistory() const;
 
-        std::default_random_engine generator;
-        std::normal_distribution<double> distribution;
+    /**
+     * @brief Retrieves the current price of the stock.
+     * @return The current price.
+     */
+    const double getPrice() const;
+
+    /**
+     * @brief Retrieves the name of the stock.
+     * @return The name of the stock.
+     */
+    const std::string getName() const;
+
+private:
+    std::string _name;                  /**< The name of the stock. */
+    double _start;                      /**< The initial price of the stock. */
+    double _mean;                       /**< The mean value for generating price changes. */
+    double _stddev;                     /**< The standard deviation for generating price changes. */
+    int _timespan;                      /**< The number of price updates to simulate. */
+    std::list<double> _price_history;   /**< The history of stock prices. */
+
+    std::default_random_engine generator;              /**< The random number generator. */
+    std::normal_distribution<double> distribution;     /**< The normal distribution for generating price changes. */
 };
