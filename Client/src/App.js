a96552a29fe4a2c7784d54ba5bbb1ecbb0feedd1
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-06-20 02:12:17 +0200

    tiny bit of infrastructure :D

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,34 @@
+import { Route, Routes } from 'react-router-dom';
+import pages from './utils/pages';
+import Layout from './components/site-layout/Layout';
+
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import ShopBuy from './components/pages/ShopBuy';
+import ShopSell from './components/pages/ShopSell';
+
 function App() {
 	return (
-		<>
-			yo
-		</>
+		<Layout>
+			<Routes>
+				<Route
+					path={ pages.get('home').path }
+					element={ <Home /> }
+				/>
+				<Route
+					path={ pages.get('login').path }
+					element={ <Login /> }
+				/>
+				<Route
+					path={ pages.get('shop-buy').path }
+					element={ <ShopBuy /> }
+				/>
+				<Route
+					path={ pages.get('shop-sell').path }
+					element={ <ShopSell /> }
+				/>
+			</Routes>
+		</Layout>
 	);
 }
 
---
+++ Client/src/components/pages/ShopSell/index.js
@@ -0,0 +1,7 @@
+export default function ShopSell() {
+    return(
+        <>
+            This is the Shop Sell Page!
+        </>
+    )
+}
