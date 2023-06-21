7c17c140424382ee69a86e5fb02af9901fa2cec6
Christian Gördes <christian.goerdes@outlook.de>
2023-06-21 15:55:59 +0200

    .

--- Server/api/api.py
+++ Server/api/api.py
@@ -117,7 +117,6 @@ def get_my_stocks(name, password):
         stock_obj = {
             "name": stock.getName(),
             "price": stock.getPrice(),
-            "amount": acc.get_stocks()[stock.getName()],
             "plot": base64.b64encode(generate_plot(stock.getPriceHistory())).decode('utf-8')
         }
         stocks.append(stock_obj)
