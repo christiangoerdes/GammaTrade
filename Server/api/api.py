97a9ca2b69cb599116e308ada2db2165213a46d8
Christian Gördes <christian.goerdes@outlook.de>
2023-06-21 15:59:08 +0200

    .

--- Server/api/api.py
+++ Server/api/api.py
@@ -111,12 +111,12 @@ def get_stocks():
 # Get the stocks for an account 
 def get_my_stocks(name, password):
     s = g.get_stocks_for(name, password)
-    acc = g.get_account(name, password)
     stocks = []
     for stock in s:
         stock_obj = {
             "name": stock.getName(),
             "price": stock.getPrice(),
+            "amount": g.get_account(name, password).get_stocks()[stock.getName()],
             "plot": base64.b64encode(generate_plot(stock.getPriceHistory())).decode('utf-8')
         }
         stocks.append(stock_obj)
