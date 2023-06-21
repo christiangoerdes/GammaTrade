f1038fccc43862a03347828b5e4e7aaf7fcde013
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-06-22 01:16:54 +0200

    updated homepage

--- Client/src/components/pages/Home/index.js
+++ Client/src/components/pages/Home/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 import { useState, useContext } from 'react';
 import { AuthContext } from '../../../utils/AuthContext';
+import ItemCard from "../Shop/ItemCard";
 
 export default function Home() {
     const { isLoggedIn, login, logout, logInName, setLogInName, logInPassword, setLogInPassword } = useContext(AuthContext);
@@ -8,7 +9,17 @@ export default function Home() {
     return(
         <>
             <div className="home-headline">
-                <h2>Home</h2>
+                <h2>Market</h2>
+            </div>
+            <div className='container home'>
+                    <p className='home-heading'>
+                        Hottest Stocks!!!
+                    </p>
+                    <div className='home-stocks'>
+                        <ItemCard stock={"DogeCoin"} />
+                        <ItemCard stock={"RedPanda Retail"} />
+                        <ItemCard stock={"Quantum Computing Inc"} />
+                    </div>
             </div>
         </>
     )
--- Client/src/utils/pages.js
+++ Client/src/utils/pages.js
@@ -2,7 +2,7 @@ const pages = new Map();
 
 pages.set('home', { name: 'Home', path: '/', anchorable: true });
 
-pages.set('shop', { name: 'Shop', path: '/shop', anchorable: true });
+pages.set('shop', { name: 'Market', path: '/shop', anchorable: true });
 
 pages.set('login', { name: 'Login', path: '/login', anchorable: false });
 
