98ff6b284deeb143a324ef728bb583b641a07b0d
Christian Gördes <christian.goerdes@outlook.de>
2023-06-21 16:04:11 +0200

    added account to pybind

--- Server/api/pybind.cpp
+++ Server/api/pybind.cpp
@@ -5,7 +5,18 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(gammaTrade, m) {
-     // Map the Stock class
+    // Map the Account class
+    py::class_<Account>(m, "Account")
+        .def(py::init<std::string, std::string>())
+        .def("add_to_balance", &Account::add_to_balance)
+        .def("take_from_balance", &Account::take_from_balance)
+        .def("buy_stock", &Account::buy_stock)
+        .def("sell_stock", &Account::sell_stock)
+        .def("get_name", &Account::get_name)
+        .def("get_password", &Account::get_password)
+        .def("get_balance", &Account::get_balance)
+        .def("get_stocks", &Account::get_stocks);
+    // Map the Stock class
     py::class_<Stock>(m, "Stock")
         .def(py::init<const std::string&, const double, const double, const double, const int, unsigned>())
         .def(py::init<>())
